7b3bd70b78e4df335195511c3fc3f1cd5296dd7a
Eloizy Campi <campieloizy80@gmail.com>
2022-11-26 17:31:37 -0400

    alterações

--- PDO/Classes/Conexao.class.php
+++ PDO/Classes/Conexao.class.php
@@ -13,10 +13,10 @@ final class Conexao{
             define('db_host', 'localhost');
         }
         if(!defined('db_pass')){
-            define('db_pass', 'llobeauchamp');
+            define('db_pass', 'Danilo@123');
         }
         if(!defined('db_name')){
-            define('db_name', 'AulaTec');
+            define('db_name', 'pw');
         }
         if(!defined('db_port')){
             define('db_port', 3306);
@@ -24,7 +24,7 @@ final class Conexao{
         try{
             $conn = new PDO('mysql:host='.db_host.'; port='.db_port.'; dbname='.db_name, db_user, db_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET NAMES utf8"));
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
         }catch (PDOException $e){
             echo 'Erro ao conectar, erro: '.$e->getMessage();
         }
